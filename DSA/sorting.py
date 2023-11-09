55dd0a7bb9d3050c90c1d9912e464ec45949b1f6
Aditya Dayal <adeedayal@gmail.com>
2023-11-09 09:34:20 +0530

    added quick and small code fro quick sort

--- DSA/sorting.py
+++ DSA/sorting.py
@@ -21,34 +21,47 @@ def insertion_sort(l):
             j -= 1
     return l
 
+
 def merge(A, B):
-    m,n=len(A), len(B)
-    C, i,j,k=[],0,0,0
-    while(k<m+n):
-        if i==m:
+    m, n = len(A), len(B)
+    C, i, j, k = [], 0, 0, 0
+    while k < m + n:
+        if i == m:
             C.extend(B[j:])
-            k+=n-j
-        elif j==n:
+            k += n - j
+        elif j == n:
             C.extend(A[i:])
-            k+=n-i
-        elif A[i]<B[j]:
+            k += n - i
+        elif A[i] < B[j]:
             C.append(A[i])
-            i,k=i+1,k+1
+            i, k = i + 1, k + 1
         else:
             C.append(B[j])
-            j,k=j+1,k+1
+            j, k = j + 1, k + 1
     return C
 
+
 def merge_sort(l):
-    n=len(l)
-    if n<=1:
+    n = len(l)
+    if n <= 1:
         return l
-    L=merge_sort(l[:n//2])
-    R=merge_sort(l[n//2:])
-    B=merge(L, R)
+    L = merge_sort(l[: n // 2])
+    R = merge_sort(l[n // 2 :])
+    B = merge(L, R)
     return B
+
+
+def quick_sort(l):
+    if len(l) <= 1:
+        return l
+    pivot = l[0]
+    left = [x for x in l[1:] if x < pivot]
+    right = [x for x in l[1:] if x >= pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
 
 l = [3, 1, 5, 4, 2]
 # print(selection_sort(l))
 # print(insertion_sort(l))
-print(merge_sort(l))
+# print(merge_sort(l))
+print(quick_sort(l))
