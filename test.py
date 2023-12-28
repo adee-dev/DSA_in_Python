3f115824b0c736bb187a9c5db5da56ddef83c620
Aditya Dayal <adeedayal@gmail.com>
2023-12-28 20:19:56 +0530

    todays peactice done

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 def binary_search(l, k):
-    if len(l) == 0:
+    n = len(l)
+    if n == 0:
         return False
-    mid = len(l) // 2
+    mid = n // 2
     if l[mid] == k:
         return True
     elif l[mid] < k:
@@ -17,67 +18,62 @@ def selection_sort(l):
     for i in range(n):
         minpos = i
         for j in range(i + 1, n):
-            if l[minpos] > l[j]:
-                l[minpos], l[j] = l[j], l[minpos]
+            if l[j] < l[minpos]:
+                l[j], l[minpos] = l[minpos], l[j]
     return l
 
 
 def insertion_sort(l):
     n = len(l)
-    if n == 0:
+    if n==0:
         return l
     for i in range(n):
-        j = i
-        while j > 0 and l[j] < l[j - 1]:
-            l[j], l[j - 1] = l[j - 1], l[j]
-            j -= 1
+        j=i
+        while j>0 and l[j]<l[j-1]:
+            l[j],l[j-1]=l[j-1],l[j]
+            j-=1
     return l
 
-
-def merge(A, B):
-    m, n = len(A), len(B)
-    C, i, j, k = [], 0, 0, 0
-    while k < m + n:
-        if i == m:
+def merge(A,B):
+    m,n=len(A),len(B)
+    C,i,j,k=[],0,0,0
+    while(k<m+n):
+        if i==m:
             C.extend(B[j:])
-            k += n - j
-        elif j == n:
+            k+=n-j
+        elif j==n:
             C.extend(A[i:])
-            k += m - i
-        elif A[i] < B[j]:
+            k+=m-i
+        elif A[i]<B[j]:
             C.append(A[i])
-            k, i = k + 1, i + 1
+            i,k=i+1,k+1
         else:
             C.append(B[j])
-            j, k = j + 1, k + 1
+            j,k=j+1,k+1
     return C
 
-
 def merge_sort(l):
-    n = len(l)
-    if n <= 1:
+    n=len(l)
+    if n<=1:
         return l
-    L = merge_sort(l[n // 2 :])
-    R = merge_sort(l[: n // 2])
-    ll = merge(L, R)
+    L,R=merge_sort(l[:n//2]),merge_sort(l[n//2:])
+    ll=merge(L,R)
     return ll
 
-
 def quick_sort(l):
-    if len(l)<=1:
+    n=len(l)
+    if n<=1:
         return l
-    else:
-        pivot=l[0]
-        left=[x for x in l[1:] if x<pivot]
-        right=[x for x in l[1:] if x>=pivot]
-        return quick_sort(left)+[pivot]+quick_sort(right)
-
+    pivot=l[0]
+    L=[x for x in l[1:] if x<pivot]
+    R=[x for x in l[1:] if x>=pivot]
+    return quick_sort(L)+[pivot]+quick_sort(R)
 
 # l=list(range(10))
-l = [2, 9, 6, 3, 7, 1, 8, 5, 4, 0]
-k = 1
+l = [6, 2, 1, 7, 4, 9, 3, 0, 8, 5]
+k = 7
 # print(selection_sort(l))
 # print(insertion_sort(l))
 # print(merge_sort(l))
 print(quick_sort(l))
-# print(binary_search(l,k))
+# print(binary_search(l, k))
